a2e832d8a280dec3ce73effb286dd62dc5339f6b
pokavv <okaps@hanmail.net>
2023-05-05 01:05:47 +0900

    230505 updated

--- beautiful_soup/main.py
+++ beautiful_soup/main.py
@@ -3,8 +3,25 @@
 from bs4 import BeautifulSoup
 import lxml
 
-with open('website.html', 'rt', encoding='UTF8') as file:
+with open('beautiful_soup/website.html', 'rt', encoding='UTF8') as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, 'lxml')
 
+all_anchor_tags = soup.find_all(name='a')
+for tag in all_anchor_tags:
+    print(tag.getText())
+    print(tag.get('href'))
+    print()
+
+heading = soup.find(name='h1', id='name')
+print(heading.getText())
+
+section_heading = soup.find(name='h3', class_='heading')
+print(section_heading.getText())
+
+name = soup.select_one(selector='#name')
+print(name)
+
+headings = soup.select('.heading')
+print(headings)
